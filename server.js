f697720edd915cbac15dd76389d1321696e2a350
coreyarch1234 <harrilal@mit.edu>
2017-08-04 16:08:48 -0700

    installed packages and set up initial post request

---
+++ server.js
@@ -0,0 +1,28 @@
+//Middleware
+
+//Express
+var express = require('express');
+var exphbs  = require('express-handlebars');
+//App
+var app = express();
+//To parse post requests
+var bodyParser = require('body-parser');
+//port 3000
+var port = 3000;
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Setting templating engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+//route to handle iOS post request
+app.post('/', function(req,res){
+    console.log("Post Success");
+    console.log(req.body);
+});
