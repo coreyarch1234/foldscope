c5b919410bb9822d5d1e7257b8320c7b70a8a78a
coreyarch1234 <harrilal@mit.edu>
2018-01-17 07:26:44 -0800

    lyrics test

--- server.js
+++ server.js
@@ -96,41 +96,55 @@ app.get('/start-scrape', (req, res) => {
     //     console.log("The file was saved!");
     // })
 
-    tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
-      if (err) throw err;
+    // tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
+    //   if (err) throw err;
+    //
+    //   console.log('File: ', path);
+    //   console.log('Filedescriptor: ', fd);
+    //
+    //   console.log("****** wrote to temp *****")
+    //
+    //   fs.writeFile(path, 'MOVING CONTENT TO PRODUCTION', function(err){
+    //       if(err){
+    //           return console.log(err);
+    //       }
+    //       console.log(" ----- wrote to path ------");
+    //       //move to my own
+    //       mv(path, './htmlTest.js', function(err) {
+    //           // done. it tried fs.rename first, and then falls back to
+    //           // piping the source file to the dest file and then unlinking
+    //           // the source file.
+    //           if (err) {
+    //               console.log(err);
+    //               return
+    //           }
+    //           console.log("&&&&&& moved to folder &&&&&& ")
+    //           cleanupCallback();
+    //       });
+    //
+    //       // cleanupCallback();
+    //       // console.log("The file was saved!");
+    //   })
+    //
+    //
+    //   // If we don't need the file anymore we could manually call the cleanupCallback
+    //   // But that is not necessary if we didn't pass the keep option because the library
+    //   // will clean after itself.
+    //   // cleanupCallback();
+    //
+    //
+    // });
+    let lyrics = 'But still I\'m having memories of high speeds when the cops crashed\n' +
+             'As I laugh, pushin the gas while my Glocks blast\n' +
+             'We was young and we was dumb but we had heart';
 
-      console.log('File: ', path);
-      console.log('Filedescriptor: ', fd);
+    // write to a new file named 2pac.txt
+    fs.writeFile('./2pac.txt', lyrics, (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
 
-      console.log("****** wrote to temp *****")
-
-      fs.writeFile(path, 'MOVING CONTENT TO PRODUCTION', function(err){
-          if(err){
-              return console.log(err);
-          }
-          console.log(" ----- wrote to path ------");
-          //move to my own
-          mv(path, './htmlTest.js', function(err) {
-              // done. it tried fs.rename first, and then falls back to
-              // piping the source file to the dest file and then unlinking
-              // the source file.
-              if (err) {
-                  console.log(err);
-                  return
-              }
-              console.log("&&&&&& moved to folder &&&&&& ")
-              cleanupCallback();
-          });
-
-          // cleanupCallback();
-          // console.log("The file was saved!");
-      })
-
-
-      // If we don't need the file anymore we could manually call the cleanupCallback
-      // But that is not necessary if we didn't pass the keep option because the library
-      // will clean after itself.
-      // cleanupCallback();
+        // success case, the file was saved
+        console.log('Lyric saved!');
     });
 })
 //serves static static
